eebe37085df489dd39f763bfd146d18a996645a7
= <=>
2024-09-05 22:19:13 +0200

    Squelette du TD

---
+++ index.js
@@ -0,0 +1,6 @@
+let main = document.getElementById( "main" ) ;
+main.innerHTML = "<h1>TD de programmation orientée objet en JavaScript</h1>" ;
+setTimeout( ()=>
+{
+    main.innerHTML = "" ;
+},3000 ) ;
