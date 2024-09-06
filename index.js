9493eaf581390dfcca48dfd7bb91017def50b0a7
Axel Rinaldi <axel.rinaldi@wanadoo.fr>
2024-09-06 09:16:33 +0200

    Stockage d'un étudiant avec des variables

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-// Ecrire un message dans la console du navigateur
-console.log( "Message sur la console") ;
+// Stoker les informations d'un étudiant
+let nom = "DELON" ;
+let prenom = "Alain" ;
+let civilite = "Monsieur" ;
+let age = 85 ;
+let section = "cinéma" ;
+
+// Affiche les informations de l'étudiant
+console.log(civilite + " " + nom + " " + prenom);
