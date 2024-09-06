f93a5777d2573add3f20af85a05aec8a8a7ab7f4
Axel Rinaldi <arinaldi7@sio.local>
2024-09-06 08:39:27 +0200

    Création du projet TD Objet JS

--- index.js
+++
@@ -1,6 +0,0 @@
-let main = document.getElementById( "main" ) ;
-main.innerHTML = "<h1>TD de programmation orientée objet en JavaScript</h1>" ;
-setTimeout( ()=>
-{
-    main.innerHTML = "" ;
-},3000 ) ;
