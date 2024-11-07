77dda4d40ee7f5ba0a533f3c9097dee0311c0b56
Axel Rinaldi <axel.rinaldi@wanadoo.fr>
2024-11-07 15:54:43 +0100

    Creation du web component <mon-wc> associé à la classe MonWC

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+class MonWC extends HTMLElement
+{
+    constructor()
+    {
+        super() ;
+    }
+
+    connectedCallback()
+    {
+        console.log( "Creation d'un element de type MonWC" ) ;
+    }
+
+}
+
+// Enregistre le web component dans JS
+customElements.define( "mon-wc", MonWC ) ;
